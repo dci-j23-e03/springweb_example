b158831e90dd61a8f636165b037cabc4b535eba3
dzenang <dzenan.granulo@gmail.com>
2024-08-22 13:03:56 +0200

    Add validation example

--- src/main/java/com/example/springweb/controller/UserController.java
+++ src/main/java/com/example/springweb/controller/UserController.java
@@ -4,8 +4,10 @@ import com.example.springweb.model.Hello;
 import com.example.springweb.model.User;
 import com.example.springweb.model.Usera;
 import com.example.springweb.service.HelloService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,16 +19,18 @@ public class UserController {
 
     @GetMapping("/new")
     public String newUser(Model model){
-        User user = new User();
-        model.addAttribute("user", user);
-        System.out.println("New User: " + user);
+        model.addAttribute("user", new User());
         return "newUser";
     }
 
     @PostMapping("/save")
-    public String saveUser(User usera){
+    public String saveUser(@ModelAttribute @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            System.out.println("Validation Error Count " + bindingResult.getErrorCount());
+            return "newUser";
+        }
         // let's imagine we have persisted user object
-        System.out.println("User saved " + usera);
+        System.out.println("User saved " + user);
         return "redirect:/user/new";
     }
 }
--- src/main/java/com/example/springweb/model/User.java
+++ src/main/java/com/example/springweb/model/User.java
@@ -1,7 +1,17 @@
 package com.example.springweb.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message = "Name field is mandatory and can't be empty")
+    @Size(min = 3, max = 50, message = "Name field needs to be between 3 and 50 characters")
     private String name;
+
+    @NotNull(message = "Age field is mandatory")
+    @Min(value = 18, message = "You should be 18 or above to register")
     private int age;
 
     public String getName() {
